f5d2439723cb56b3b5867585bf8593383532eacb
boroda4436@gmail.com <zamanalumene5>
2017-03-17 00:26:10 +0200

    add tests

---
+++ src/test/java/ua/com/jon/service/EmployeeServiceTest.java
@@ -0,0 +1,57 @@
+package ua.com.jon.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import ua.com.jon.conf.AppConfig;
+import ua.com.jon.dao.ConnectorSettingsDao;
+import ua.com.jon.dao.EmployeeDao;
+import ua.com.jon.dao.impl.ConnectorSettingsDaoImpl;
+import ua.com.jon.dao.impl.EmployeeDaoImpl;
+import ua.com.jon.dto.Salary;
+import ua.com.jon.service.impl.EmployeeServiceImpl;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Bohdan on 16.03.2017
+ */
+
+@ContextConfiguration(classes = AppConfig.class)
+public class EmployeeServiceTest {
+
+    private ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+    private EmployeeService employeeService;
+
+
+    @Test
+    public void getRandomSalary() throws Exception {
+        employeeService = ctx.getBean(EmployeeService.class);
+        assertNotNull(ctx);
+        assertNotNull(employeeService);
+
+        Salary salary = employeeService.getRandomSalary();
+        assertNotNull(salary);
+    }
+
+
+    @Test
+    public void getSalaryWithMockServiceCreation() throws NoSuchFieldException, IllegalAccessException {
+        EmployeeDao employeeDao = new EmployeeDaoImpl();
+        ConnectorSettingsDao connectorSettingsDao = new ConnectorSettingsDaoImpl();
+        EmployeeService employeeService = new EmployeeServiceImpl(employeeDao, connectorSettingsDao);
+
+
+        Field employeeDaoField = employeeService.getClass().getDeclaredField("employeeDao");
+        employeeDaoField.setAccessible(true);
+        employeeDaoField.set(employeeService, employeeDao);
+
+        Salary salary = employeeService.getRandomSalary();
+        assertNotNull(salary);
+    }
+
+}
