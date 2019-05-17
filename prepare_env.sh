b7d42b2d00405f01ad0638dade01ccc23671a968
Bohdan Chupika <boroda4436@users.noreply.github.com>
2019-05-17 13:17:54 +0300

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-mkdir -p /opt/spring-dependency-injection/logs
+LOG_DIRECTORY=/opt/spring-dependency-injection/logs
+mkdir -p $LOG_DIRECTORY
+if [ ! -d "$LOG_DIRECTORY" ]; then
+  echo "ERROR: could not find log direcrtory"
+fi
